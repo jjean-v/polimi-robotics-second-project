518422eea602bc31cc13f4e56d7f686b65871972
jjean-v <14jean04@gmail.com>
2026-06-15 19:59:26 +0200

    correct nav

--- colcon_ws/src/second_project/launch/navigation_launch.py
+++ colcon_ws/src/second_project/launch/navigation_launch.py
@@ -15,7 +15,8 @@ def generate_launch_description():
     world_file = os.path.join(pkg_share, 'world', 'project.world')
     map_yaml = os.path.join(pkg_share, 'map', 'my_map.yaml')
     nav2_params = os.path.join(pkg_share, 'config', 'nav2_params_stage.yaml')
-    rviz_config = os.path.join(pkg_share, 'config', 'rviz', 'mapping_rviz_config.rviz') 
+    rviz_config = os.path.join(pkg_share, 'config', 'rviz', 'nav_rviz_config.rviz') 
+
     navigation_launch = os.path.join(nav2_bringup_dir, 'launch', 'navigation_launch.py')
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
 
@@ -93,13 +94,13 @@ def generate_launch_description():
         }],
     )
 
-    # rviz_node = Node(
-    #     package='rviz2',
-    #     executable='rviz2',
-    #     name='rviz2',
-    #     arguments=['-d', rviz_config],
-    #     parameters=[{'use_sim_time': use_sim_time}]
-    # )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments=['-d', rviz_config],
+        parameters=[{'use_sim_time': use_sim_time}]
+    )
 
     return LaunchDescription([
         DeclareLaunchArgument('use_sim_time', default_value='true', description='Use the simulator /clock.'),
@@ -111,8 +112,8 @@ def generate_launch_description():
         DeclareLaunchArgument('map_frame', default_value='map', description='Global map frame.'),
         DeclareLaunchArgument('base_frame', default_value='base_footprint', description='Robot base frame.'),
         DeclareLaunchArgument('set_initial_pose', default_value='true', description='Let AMCL initialize from initial_x, initial_y, initial_yaw. Set false to use RViz 2D Pose Estimate.'),
-        DeclareLaunchArgument('initial_x', default_value='2.0', description='Initial robot x in the map frame.'),
-        DeclareLaunchArgument('initial_y', default_value='2.0', description='Initial robot y in the map frame.'),
+        DeclareLaunchArgument('initial_x', default_value='8.6', description='Initial robot x in the map frame.'),
+        DeclareLaunchArgument('initial_y', default_value='-4.1', description='Initial robot y in the map frame.'),
         DeclareLaunchArgument('initial_yaw', default_value='0.0', description='Initial robot yaw in radians in the map frame.'),
         DeclareLaunchArgument('nav2_params_file', default_value=nav2_params, description='Nav2 parameter file.'),
         DeclareLaunchArgument('rviz_config', default_value=rviz_config, description='RViz config.'),
@@ -122,5 +123,5 @@ def generate_launch_description():
         amcl,
         lifecycle_manager_localization,
         nav2_navigation,
-        # rviz_node
+        rviz_node
     ])
